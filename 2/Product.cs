9d2ec99830b359c46984cec432065e3a3f671cde
taeekii <terenteva1999@yandex.ru>
2024-04-19 01:55:40 +0300

    second task

---
+++ 2/Product.cs
@@ -0,0 +1,83 @@
+﻿using SecondTask;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SecondTask
+{
+    internal abstract class Product
+    {
+        protected string Name;
+        protected int Price;
+        protected int ExpirationDate;
+        protected DateTime DayOfManufacture;
+
+        public virtual string Info()
+        {
+            return string.Format("Название продукта - {0}\nЦена - {1}", Name, Price);
+        }
+
+        public virtual bool IsCorrespondsToExpire(DateTime currentDate)
+        {
+            return (currentDate < DayOfManufacture + new TimeSpan(ExpirationDate, 0, 0, 0));
+        }
+    }
+    internal class Commodity : Product
+    {
+        public Commodity(string name, int price, DateTime dayofman, int expirat)
+        {
+            Name = name;
+            Price = price;
+            DayOfManufacture = dayofman;
+            ExpirationDate = expirat;
+        }
+
+        public override string Info()
+        {
+            return base.Info() +
+                string.Format("\nДата производства - {0}\nСрок годности - {1} дней",
+                              DayOfManufacture, ExpirationDate);
+
+        }
+    }
+
+    internal class Batch : Product
+    {
+        private readonly int _count;
+
+        public Batch(string name, int price, int count, DateTime dateofman, int expir)
+        {
+            Name = name;
+            Price = price;
+            _count = count;
+            DayOfManufacture = dateofman;
+            ExpirationDate = expir;
+        }
+        public override string Info()
+        {
+            return base.Info() +
+                   string.Format("\nКоличество - {0}\nДата производства - {1}\nСрок годности - {2} дней",
+                                 _count, DayOfManufacture, ExpirationDate);
+        }
+    }
+
+    internal class Complect : Product
+    {
+        private readonly string _list;
+
+        public Complect(string name, int price, string list)
+        {
+            Name = name;
+            Price = price;
+            _list = list;
+        }
+        public override bool IsCorrespondsToExpire(DateTime currentDate)
+        {
+            return true;
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using SecondTask;
+
+Product[] product = new Product[6];
+product[0] = new Commodity("Йогурт", 59, Convert.ToDateTime("02.04.2024"), 30);
+product[1] = new Commodity("Молоко", 109, Convert.ToDateTime("12.03.2024"), 10);
+product[2] = new Batch("Сырки", 39, 10, Convert.ToDateTime("08.04.2024"), 20);
+product[3] = new Batch("Батончики", 99, 50, Convert.ToDateTime("01.01.2024"), 100);
+product[4] = new Complect("Ложки", 39, "Столовые");
+product[5] = new Complect("Ложки", 15, "Десертные");
+
+DateTime now = DateTime.Now;
+
+foreach (var prod in product)
+{
+    Console.WriteLine(prod.Info());
+    Console.WriteLine(prod.IsCorrespondsToExpire(now) ? "Не просрочен" : "Просрочен");
+    Console.WriteLine();
+}
